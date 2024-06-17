6587fb8f5f22fad66d9b08ad51f02edb131fa02d
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-06-17 16:44:25 +0200

    Init

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        evmVersion: "istanbul",
+      },
+    },
+  },
+
+  mocha: {
+    timeout: 100000,
+  },
+};
