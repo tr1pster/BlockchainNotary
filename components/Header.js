0eb3353d1394c434ec17a988733314c03c85a2b5
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-07-28 11:46:55 +0200

    add error handling

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,59 @@
 import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, errorMessage: '' };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, errorMessage: error.toString() };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
 
 const Header = () => {
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">BlockchainNotary</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink to="/" exact activeClassName="active" className="nav-link">Home</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/verify" activeClassName="active" className="nav-link">Verify</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/notarize" activeClassName="active" className="nav-link">Notarize</NavLink>
-              </li>
-            </ul>
+    <ErrorBoundary>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">BlockchainNotary</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink to="/" exact activeClassName="active" className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/verify" activeClassName="active" className="nav-link">Verify</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/notarize" activeClassName="active" className="nav-link">Notarize</NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </ErrorBoundary>
   );
 };
 
