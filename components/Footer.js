2894d2395a5cdf8e567ec7bf2addd102661b973b
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-08-03 14:10:57 +0200

    optimize the naming

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
-const Footer = () => {
+const BlockchainNotaryFooter = () => {
+  const twitterHandle = process.env.TWITTER_HANDLE;
+  const facebookHandle = process.env.FACEBOOK_HANDLE;
+  const instagramHandle = process.env.INSTAGRAM_HANDLE;
+
   return (
-    <footer style={{background: "#f0f0f0", textAlign: "center", padding: "20px", marginTop: "20px"}}>
+    <footer style={{ background: "#f0f0f0", textAlign: "center", padding: "20px", marginTop: "20px" }}>
       <p>© 2023 BlockchainNotary. All rights reserved.</p>
+
       <p>
         <a href="#">Home</a> | 
         <a href="#about">About Us</a> | 
         <a href="#services">Services</a> | 
         <a href="#contact">Contact Us</a>
       </p>
+
       <p>
-        Follow us on 
-        <a href={`https://twitter.com/${process.env.TWITTER_HANDLE}`}> Twitter</a>, 
-        <a href={`https://www.facebook.com/${process.env.FACEBOOK_HANDLE}`}> Facebook</a>, and 
-        <a href={`https://www.instagram.com/${process.env.INSTAGRAM_HANDLE}`}> Instagram</a>
+        Follow us on
+        <a href={`https://twitter.com/${twitterHandle}`}> Twitter</a>, 
+        <a href={`https://www.facebook.com/${facebookHandle}`}> Facebook</a>, and 
+        <a href={`https://www.instagram.com/${instagramHandle}`}> Instagram</a>
       </p>
     </footer>
   );
 }
 
-export default Footer;
+export default BlockchainNotaryFooter;
