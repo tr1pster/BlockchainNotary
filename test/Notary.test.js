1beab978eb00389d710ff580e1700a1592413715
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-09-15 12:44:14 +0200

    change names

--- test/Notary.test.js
+++ test/Notary.test.js
@@ -3,46 +3,47 @@ const chai = require("chai");
 const { expect } = chai;
 chai.should();
 
-contract("Notary", accounts => {
-  let notaryContract;
-  const documentHash = "0xabcdef1234567890000000000000000000000000000000000000000000000000";
-  const documentSigner = accounts[1];
+contract("Notary", (accountAddresses) => {
+  let notaryInstance;
+  const sampleDocumentHash = "0xabcdef1234567890000000000000000000000000000000000000000000000000";
+  const signerAccount = accountAddresses[1];
 
   before(async () => {
-    notaryContract = await Notary.deployed();
+    notaryInstance = await Notary.deployed();
   });
 
-  describe("Registering a Document", () => {
-    it("should emit a DocumentRegistered event on successful registration", async () => {
-      const tx = await notaryContract.registerDocument(documentHash, { from: documentSigner });
-      expect(tx.logs[0].event).to.equal("DocumentRegistered");
-      expect(tx.logs[0].args.docHash).to.equal(documentHash);
+  describe("Document Registration", () => {
+    it("should emit a DocumentRegistered event upon successful registration", async () => {
+      const registrationTx = await notaryInstance.registerDocument(sampleDocumentHash, { from: signerAccount });
+      expect(registrationTx.logs[0].event).to.equal("DocumentRegistered");
+      expect(registrationTx.logs[0].args.documentHash).to.equal(sampleDocumentHash);
     });
   });
 
-  describe("Signing a Document", () => {
-    it("should emit a DocumentSigned event on successful signing", async () => {
-      const tx = await notaryContract.signDocument(documentHash, { from: documentSigner });
-      expect(tx.logs[0].event).to.equal("DocumentSigned");
-      expect(tx.logs[0].args.docHash).to.equal(documentHash);
-      expect(tx.logs[0].args.signer).to.equal(documentSigner);
+  describe("Document Signing", () => {
+    it("should emit a DocumentSigned event upon successful signing", async () => {
+      const signingTx = await notaryInstance.signDocument(sampleDocumentHash, { from: signerAccount });
+      expect(signingTx.logs[0].event).to.equal("DocumentSigned");
+      expect(signingTx.logs[0].args.documentHash).to.equal(sampleDocumentHash);
+      expect(signingTx.logs[0].args.signerAddress).to.equal(signerAccount);
     });
   });
 
-  describe("Verifying a Document", () => {
-    it("should verify a registered and signed document successfully", async () => {
-      const registered = await notaryContract.isDocumentRegistered(documentHash);
-      const signed = await notaryContract.isDocumentSigned(documentHash, { from: documentSigner });
+  describe("Document Verification", () => {
+    it("should confirm the registration and signing of a document", async () => {
+      const isRegistered = await notaryInstance.isDocumentRegistered(sampleDocumentHash);
+      const isSigned = await notaryInstance.isDocumentSigned(sampleDocumentHash, { from: signerAccount });
 
-      registered.should.be.true;
-      signed.should.be.true;
+      isRegistered.should.be.true;
+      isSigned.should.be.true;
     });
 
-    it("should not verify an unsigned document", async () => {
-      const newDocHash = "0xdecafbad12345678900000000000000000000000000000000000000000000000";
-      await notaryContract.registerDocument(newDocHash, { from: accounts[2] });
-      const signed = await notaryContract.isDocumentSigned(newDocHash, { from: accounts[2] });
-      signed.should.be.false;
+    it("should reject verification for an unsigned document", async () => {
+      const newDocumentHash = "0xdecafbad12345678900000000000000000000000000000000000000000000000";
+      await notaryInstance.registerDocument(newDocumentHash, { from: accountAddresses[2] });
+      const isSignedForNewDoc = await notaryInstance.isDocumentSigned(newDocumentHash, { from: accountAddresses[2] });
+
+      isSignedForNewDoc.should.be.false;
     });
   });
 });
